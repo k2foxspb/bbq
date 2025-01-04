7962e78a52acca16315af98e44e1c7a5ffab5c89
k2fox <k2foxspb@mail.ru>
2025-01-04 16:13:24 +0300

    basket

--- bbq/basket/telegram.py
+++ bbq/basket/telegram.py
@@ -6,7 +6,6 @@ bot = telebot.TeleBot('7013936598:AAG9HKVZoltQnDfoz0UAUJoziWOrgm-FhA0')
 
 # from telebot import types
 def send_message(message):
-    chat_id = 939130884
-    chat_id_2 =  847678097              # Армен
+
+    chat_id_2 = 847678097              # Армен
     bot.send_message(chat_id_2, message)
-    bot.send_message(chat_id, message)
