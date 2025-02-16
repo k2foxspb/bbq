e31303479644d1a78a7620d864a0be48d6e6921a
k2fox <k2foxspb@mail.ru>
2025-02-16 23:27:37 +0300

    basket

--- bbq/basket/models.py
+++ bbq/basket/models.py
@@ -21,7 +21,7 @@ def unique_slugify(instance, slug):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50,blank=True,)
     def __str__(self):
         return self.name
