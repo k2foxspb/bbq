bc28c173582e6df1b30da557a798d43525d72253
k2fox <k2foxspb@mail.ru>
2025-10-31 08:19:34 +0300

    бля(

--- bbq/bbq/urls.py
+++ bbq/bbq/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 # Always serve media files through Django (not recommended for production, but works for now)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
